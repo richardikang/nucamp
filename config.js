30a24b09adfd3a4682c1876a1cbb9068e72feecd
richardikang <76178368+richardikang@users.noreply.github.com>
2021-07-13 15:27:53 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '3087556974812670',
+        clientSecret: '7362a402be27a43d7ddf833f9a6f0136'
+    }
+}
